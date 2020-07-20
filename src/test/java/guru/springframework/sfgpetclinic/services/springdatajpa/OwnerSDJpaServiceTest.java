55788a804749aaf44140f0c894713a7b3bd6a84b
hanseld28 <hansel.donizete@gmail.com>
2020-07-20 12:33:14 -0300

    feat: implement skip specific test and test class

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -0,0 +1,46 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIf;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Disabled(value = "Disabled until we learn Mocking")
+class OwnerSDJpaServiceTest {
+
+    OwnerSDJpaService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new OwnerSDJpaService(null, null, null);
+    }
+
+    @Disabled
+    @Test
+    void findByLastName() {
+        Owner owner = service.findByLastName("Mckey");
+    }
+
+    @Test
+    void findAllByLastNameLike() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
